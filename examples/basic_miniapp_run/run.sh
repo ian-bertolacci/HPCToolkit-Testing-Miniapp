6347656a15319d324d9ec00ac1e793925cf1319e
Ian Bertolacci <ian.bertolacci@gmail.com>
2020-08-06 20:04:36 -0700

    More example changes

--- examples/basic_miniapp_run/run.sh
+++ examples/basic_miniapp_run/run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-# Test un arguments
+# Usage:
+##   ./run.sh
+
+# Basic test arguments
+## The default configuration used here is:
+### Run with 10000 elements and 10000 iterations (This is about how much work is needed to start seeing *any* measurements in the profile)
+### Run with 2 mpi process and 2 OpenMP thread.
+### Measure CPUTIME at the event's standard rate.
+
 ## Problem/work configuration
 elements=10000
 iterations=10000
@@ -13,7 +21,8 @@ threads=2
 ## Profiling event
 event="CPUTIME"
 
-# More detailed setup
+# Actual setup of commands
+## Setup miniapp command
 miniapp=../../miniapp/miniapp.exe
 miniapp_source=$(dirname ${miniapp})
 miniapp_name=$(basename ${miniapp})
@@ -24,22 +33,26 @@ miniapp_threads=${threads}
 miniapp_args="-n ${miniapp_n_elements} -i ${miniapp_iterations} -d ${miniapp_work_distribution} -t ${miniapp_threads}"
 miniapp_command="${miniapp} ${miniapp_args}"
 
+## Setting up MPI Command
 mpi_spawn=mpirun
 mpi_processes=${processes}
 mpi_args="--oversubscribe -np ${mpi_processes}"
 mpi_base_command="${mpi_spawn} ${mpi_args}"
 
+## Setting up hpcstruct (static binary analysis) command
 hpcstruct=hpcstruct
 hpcstruct_output="./${miniapp_name}.hpcstruct"
 hpcstruct_args="-o ${hpcstruct_output}"
 hpcstruct_command="${hpcstruct} ${miniapp} ${hpcstruct_args}"
 
+## Setting up hpcrun (dynamic performance profiling) command
 hpcrun=hpcrun
 hpcrun_event=${event}
 hpcrun_output="${miniapp_name}.hpcmeasurements"
 hpcrun_args="-t -o ${hpcrun_output} -e ${hpcrun_event}"
 hpcrun_command="${mpi_base_command} ${hpcrun} ${hpcrun_args} ${miniapp_command}"
 
+## Setting up hpcpfor (performance profile datababase creation) command
 hpcprof=hpcprof
 hpcprof_output="${miniapp_name}.hpcdatabase"
 hpcprof_args="-S ${hpcstruct_output} -I ./+ ${hpcrun_output} -o ${hpcprof_output}"
@@ -52,20 +65,21 @@ then
   exit -1
 fi
 
-# Make hpcstruct object (miniapp.hpcstruct)
+# Performing whole analysis and profiling
+## Make hpcstruct object (creating the file miniapp.exe.hpcstruct)
 echo -e "Performing binary analysis:\n${hpcstruct_command}"
 eval ${hpcstruct_command}
 
-# Perform profiling (creating measurements directory)
+## Perform profiling (creating the directory miniapp_name.exe.hpcmeasurements containing the profile measurements)
 echo -e "\nPerforming Profiling analysis:\n${hpcrun_command}"
 eval ${hpcrun_command}
 
-# Make database
+## Make performance profiling database (creating the directory miniapp_name.exe.hpcdatabase containing the profile database)
 echo -e "\nCreating performance database:\n${hpcprof_command}"
 eval ${hpcprof_command}
 
-# Output can be viewed with hpcviewer and hpctraceviewer
-# Source view
-#   hpcviewer miniapp.exe.hpcdatabase
-# Trace view
-#   hpctraceviewer miniapp.exe.hpcdatabase
+# Output can be viewed with hpcviewer and hpctraceviewer:
+## Source view:
+###   hpcviewer miniapp.exe.hpcdatabase
+## Trace view:
+###   hpctraceviewer miniapp.exe.hpcdatabase
--- examples/basic_miniapp_run/simple_run.sh
+++ examples/basic_miniapp_run/simple_run.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-# Make hpcstruct object (miniapp.hpcstruct)
-echo "Performing binary analysis"
-hpcstruct ../../miniapp/miniapp.exe -o miniapp.hpcstruct
+# Usage:
+##   ./simple_run.sh
 
-# Perform profiling (creating measurements directory)
-# Run with 10000 elements and 10000 iterations (This is about how much work is needed to start seeing *any* measurements)
-# Run with 2 mpi process and 2 OpenMP thread
-# Measure CPUTIME
+# Make hpcstruct object (creating the file miniapp.exe.hpcstruct)
+echo "Performing binary analysis"
+hpcstruct ../../miniapp/miniapp.exe -o miniapp.exe.hpcstruct
+
+# Perform profiling (creating the directory miniapp_name.exe.hpcmeasurements containing the profile measurements)
+## Run with 10000 elements and 10000 iterations (This is about how much work is needed to start seeing *any* measurements in the profile)
+## Run with 2 mpi process and 2 OpenMP thread
+## Measure CPUTIME at the event's standard rate.
 echo "Performing profiling"
 mpirun --oversubscribe -np 2 hpcrun -t -e CPUTIME -o miniapp.exe.hpcmeasurements ../../miniapp/miniapp.exe -t 2 -n 10000 -i 10000
 
-# Make database
+# Make performance profiling database (creating the directory miniapp_name.exe.hpcdatabase containing the profile database)
 echo -e "\nCreating performance database\n"
-hpcprof -S miniapp.hpcstruct -I ../../miniapp/+ miniapp.exe.hpcmeasurements -o miniapp.exe.hpcdatabase
+hpcprof -S miniapp.exe.hpcstruct -I ../../miniapp/+ miniapp.exe.hpcmeasurements -o miniapp.exe.hpcdatabase
 
-# Output can be viewed with hpcviewer and hpctraceviewer
-# Source view
-#   hpcviewer miniapp.exe.hpcdatabase
-# Trace view
-#   hpctraceviewer miniapp.exe.hpcdatabase
+# Output can be viewed with hpcviewer and hpctraceviewer:
+## Source view:
+###   hpcviewer miniapp.exe.hpcdatabase
+## Trace view:
+###   hpctraceviewer miniapp.exe.hpcdatabase
